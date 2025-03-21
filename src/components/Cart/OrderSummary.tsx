a73d384b3af1a14433d1858944ec1a0502ec184d
George <126503204+GeorgiySergeev@users.noreply.github.com>
2025-03-22 00:17:29 +0200

    cart

--- src/components/Cart/OrderSummary.tsx
+++ src/components/Cart/OrderSummary.tsx
@@ -1,7 +1,7 @@
-import { selectTotalPrice } from "@/redux/features/cart-slice";
-import { useAppSelector } from "@/redux/store";
-import React from "react";
-import { useSelector } from "react-redux";
+import { selectTotalPrice } from '@/redux/features/cart-slice';
+import { useAppSelector } from '@/redux/store';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const OrderSummary = () => {
   const cartItems = useAppSelector((state) => state.cartReducer.items);
@@ -12,7 +12,7 @@ const OrderSummary = () => {
       {/* <!-- order list box --> */}
       <div className="bg-white shadow-1 rounded-[10px]">
         <div className="border-b border-gray-3 py-5 px-4 sm:px-8.5">
-          <h3 className="font-medium text-xl text-dark">Order Summary</h3>
+          <h3 className="font-medium text-xl text-dark">Сумма замовлення</h3>
         </div>
 
         <div className="pt-2.5 pb-8.5 px-4 sm:px-8.5">
@@ -28,13 +28,15 @@ const OrderSummary = () => {
 
           {/* <!-- product item --> */}
           {cartItems.map((item, key) => (
-            <div key={key} className="flex items-center justify-between py-5 border-b border-gray-3">
+            <div
+              key={key}
+              className="flex items-center justify-between py-5 border-b border-gray-3">
               <div>
-                <p className="text-dark">{item.title}</p>
+                <p className="text-dark">{item.name}</p>
               </div>
               <div>
                 <p className="text-dark text-right">
-                  ${item.discountedPrice * item.quantity}
+                  {/* ${item.discountedPrice * item.quantity} */}
                 </p>
               </div>
             </div>
@@ -46,17 +48,14 @@ const OrderSummary = () => {
               <p className="font-medium text-lg text-dark">Total</p>
             </div>
             <div>
-              <p className="font-medium text-lg text-dark text-right">
-                ${totalPrice}
-              </p>
+              <p className="font-medium text-lg text-dark text-right">${totalPrice}</p>
             </div>
           </div>
 
           {/* <!-- checkout button --> */}
           <button
             type="submit"
-            className="w-full flex justify-center font-medium text-white bg-blue py-3 px-6 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
-          >
+            className="w-full flex justify-center font-medium text-white bg-blue py-3 px-6 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5">
             Process to Checkout
           </button>
         </div>
